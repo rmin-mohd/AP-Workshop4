908db57f6a4f3bd602c0af30417e29ff48c08ecc
rmin-mohd <mminhak@gmail.com>
2024-11-16 20:21:41 +0330

    Class Person Done

--- VotingMachine/src/Person.java
+++ VotingMachine/src/Person.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String firstname;
+    private String lastname ;
+
+    public Person(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+    public String toString(){
+        return firstname + " " + lastname ;
+    }
 }
